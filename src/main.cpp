f3f74fff5056b766780e2df61081ace8d4542076
moosemanuk <sixbrokensoldiers@gmail.com>
2024-06-12 22:42:45 +0100

    Initialisation

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <raylib.h>
+
+
+int main()
+{
+    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(darkGreen);
+        ball.Update();
+        ball.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
